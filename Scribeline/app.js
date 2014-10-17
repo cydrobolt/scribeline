49dd8a3a8477394321e56dda8b2496ded107194d
Chaoyi Zha <summermontreal@gmail.com>
2014-10-17 17:18:28 -0400

    first commit

--- Scribeline/app.js
+++ Scribeline/app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var start = require('./routes/start');
+
 var users = require('./routes/users');
 
 var app = express();
@@ -22,8 +24,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
+ * Routings
+ *
+*/
+
+
+
 app.use('/', routes);
+app.use('/start', function(req, res) {
+  res.render('start', { title: 'Express' });
+});
+
 app.use('/users', users);
+
+
+
+/*
+ * End routings
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
